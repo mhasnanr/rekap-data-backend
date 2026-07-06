d956d82b9228babe6061b4d87c76bc21cd99062a
hasnan-regard <mhasnan.regard@oper8-ai.com>
2026-07-07 06:45:41 +0700

    fix(database): implement connection management for mongoose

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const corsOptions = require('./config/cors');
-const PORT = 8080;
 require('dotenv').config();
+
+const app = express();
 
 app.use(morgan(':method :url :status - :response-time ms'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-mongoose.connect(process.env.SITUNTING_DATABASE_URL);
+let isConnected = false;
+
+async function connectToDatabase() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.SITUNTING_DATABASE_URL);
+  isConnected = true;
+  console.log('Mongoose connected to db');
+}
+
+app.use(async (req, res, next) => {
+  await connectToDatabase();
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to Situnting Backend!');
 });
 
-// app.use('/auth', require('./routes/auth.route.js'));
 app.use('/parents', require('./routes/parent.route.js'));
 app.use('/children', require('./routes/child.route.js'));
 app.use('/records', require('./routes/record.route.js'));
@@ -26,9 +37,4 @@ app.use('/reports', require('./routes/report.route.js'));
 app.use('/summary', require('./routes/summary.route.js'));
 app.use('/immunisation', require('./routes/immunisation.route.js'));
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to db');
-  app.listen(PORT, () => {
-    console.log(`Service running on port ${PORT}`);
-  });
-});
+module.exports = app;
